894b53544eaee748b6752f141d2fb496941ae1dc
sahilkumar6006 <sahilgorka123@gmail.com>
2024-01-22 23:00:33 +0530

    hello

--- Data Structure And Algorithm/Array/mergeTwoSorted.js
+++ Data Structure And Algorithm/Array/mergeTwoSorted.js
@@ -2,16 +2,27 @@ mergeSortedArrays([0,2,4,31], [4,6,30])
 
 function mergeSortedArrays(arr1, arr2) {
   let arr = [];
- for(let i =0; i < arr1.length; i++) {
-  for(let j =0; j< arr2.length; j++) {
-    console.log(arr1[i], arr2[j]);
-    
-    if(arr[i] < arr[j]) {
-      arr[i] = arr1[i]
-    }else {
-      arr[i]  = arr2[j];
-    }
+ let firstArr1 = arr1[0];
+ let firstArr2 = arr1[0];
+ let i =1;
+ let j =1;
+ console.log(firstArr1, firstArr2);
+ if(arr1.length == 0){
+  console.log(arr2);
+ }
+if(arr2.length == 0) {
+  console.log(arr1);
+}
+ while() {
+  console.log(arr1.length, arr2.length)
+  if(arr1.length < arr2.length) {
+    arr[i].push(arr1[i])
+    i++
+  }else {
+    arr[i].push(arr2[i]);
+    j++
   }
  }
-console.log(arr);
+ console.log(arr);
 }
+
